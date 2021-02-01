e081fcc2440ff1b0f68ca112c81b72f8fef354d5
pardeep-kesnani1234 <pardeep.kesnani@live.com>
2021-02-01 12:15:40 +0500

    final1

--- Analyze_Data.py
+++ Analyze_Data.py
@@ -7,20 +7,22 @@ import pandas as pd
 import joblib
 from pandas_profiling import ProfileReport
 import streamlit.components.v1 as components
-
+import base64
+import os
 
 
 
 #INITIALIZATION OF VARIABLES
-parameter_list=['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']
-parameter_input_values=[]
-parameter_description=['Number of times pregnant','Plasma glucose concentration over 2 hours in an oral glucose tolerance test','Diastolic blood pressure (mm Hg)','Triceps skin fold thickness (mm)','2-Hour serum insulin (mu U/ml)','Body mass index (weight in kg/(height in m)2)','Diabetes pedigree function (a function which scores likelihood of diabetes based on family history)','Age (years)']
-parameter_default_values=['6','148','72','35','0','33.6','0.627','50','1']
+# parameter_list=['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']
+# parameter_input_values=[]
+# parameter_description=['Number of times pregnant','Plasma glucose concentration over 2 hours in an oral glucose tolerance test','Diastolic blood pressure (mm Hg)','Triceps skin fold thickness (mm)','2-Hour serum insulin (mu U/ml)','Body mass index (weight in kg/(height in m)2)','Diabetes pedigree function (a function which scores likelihood of diabetes based on family history)','Age (years)']
+# parameter_default_values=['6','148','72','35','0','33.6','0.627','50','1']
 
 #CREATING INTERFACE & UI INTERACTIONS
 
 st.markdown("<h1 style='text-align: center; color: Black;'>Analyze Your Data \n\n</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; color: Green;'>(By Just Your Excel Importing File) \n\n</h4>", unsafe_allow_html=True)
+
 
 
 #filename = st.text_input('Enter a file path:')
@@ -38,26 +40,39 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     return href
 
 
+# def get_table_download_link(html):
+    # """Generates a link allowing the data in a given panda dataframe to be downloaded
+    # in:  dataframe
+    # out: href string
+    # """
+    # #val = to_html(html)
+    # b64 = base64.b64encode(html)  # val looks like b'...'
+    # return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.html">Download html file</a>' # decode b'abc' => abc
 
-uploaded_file = st.file_uploader("Choose a file")
+
+
+uploaded_file = st.file_uploader("Choose a Excel File")
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+  df = pd.read_excel(uploaded_file)
+  st.markdown("<h4 style='text-align: center; color: Black;'>Display Sample Data \n\n</h4>", unsafe_allow_html=True)
+
   st.write(df.head())
   #st.write(uploaded_file)
-  profile = ProfileReport(df, title= "Profiling Report by Virtual Analyst")
   
-  profile.to_file("report.html")
+  
+  #profile.to_file("report.html")
+  
   #components.html(profile)
-  HtmlFile = open('report.html', 'r', encoding='utf-8')
-  source_code = HtmlFile.read() 
+  #HtmlFile = open('report.html', 'r', encoding='utf-8')
+  #source_code = HtmlFile.read() 
   #st.write(source_code)
-  components.html(source_code,width=500, height=500)
+  #components.html(source_code,width=500, height=500)
   #'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
 
   
    
     
-  print(profile)
+  #print(profile)
   #components.iframe(components.html(profile))  
   #components.html("<html><body><h1>Hello, World</h1></body></html>", width=200, height=200)
 
@@ -65,12 +80,17 @@ if uploaded_file is not None:
   #st.markdown(get_binary_file_downloader_html(profile, 'HTML'), unsafe_allow_html=True)
 
 
-if st.button("Click Here to Predict"):
+if st.button("Analyze Data"):
 
-    st.write('\n','\n')
-    prediction_value=predict(parameter_user_values)[0]
-    prediction_proba=str(round(float(predict(parameter_user_values)[1]),1)*100)+'%'
-    prediction='Positive' if float(prediction_value) >0.4 else 'Negative'
-
+	profile = ProfileReport(df, title= "Profiling Report by Virtual Analyst")
+	profile.to_file("report.html")
+	HtmlFile = open('report.html', 'r', encoding='utf-8')
+	source_code = HtmlFile.read() 
+	#st.write(source_code)
+	components.html(source_code,width=1000, height=1000, scrolling=True)
+	st.markdown(get_binary_file_downloader_html('report.html'), unsafe_allow_html=True)
+	
+		
+    #st.write('\n','\n')
     #DISPLAYING MODEL OUTPUT AND PREDICTION PROBABILITY 
-    st.write('Your Diabetes Prediction is:**',prediction,' **with **',prediction_proba,'** confidence')
+    #st.write('Your Diabetes Prediction is:**',prediction,' **with **',prediction_proba,'** confidence')
